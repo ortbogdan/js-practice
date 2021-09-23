1710a8aee101f2306d12bb65f45815491cf000a8
Bogdan <ortbg1988@gmail.com>
2021-09-23 13:05:45 +0300

    Update task-solving.js

--- task-solving.js
+++ task-solving.js
@@ -1506,71 +1506,71 @@ const obj = {
 // const names = [...books].filter(book => book.rating >=  MIN_BOOK_RATING).map(book => book.author).sort();
 // console.log(names);
 // ========================================================================= //46
-// const users = [
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     eyeColor: "blue",
-//     friends: ["Sharron Pace"],
-//     isActive: false,
-//     balance: 2811,
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     eyeColor: "blue",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     isActive: true,
-//     balance: 3821,
-//     gender: "female"
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     eyeColor: "green",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     isActive: false,
-//     balance: 3793,
-//     gender: "male"
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     eyeColor: "green",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     isActive: true,
-//     balance: 2278,
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     eyeColor: "blue",
-//     friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
-//     isActive: true,
-//     balance: 3951,
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     eyeColor: "brown",
-//     friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
-//     isActive: false,
-//     balance: 1498,
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     eyeColor: "brown",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     isActive: true,
-//     balance: 2764,
-//     gender: "female"
-//   }
-// ]
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
 // Дополни функцию getNamesSortedByFriendCount(users) так, чтобы она возвращала 
 // массив имён пользователей отсортированный по возрастанию количества их друзей(свойство friends).
 // const getNamesSortedByFriendCount = users => {
@@ -1590,7 +1590,8 @@ const obj = {
 // Дополни функцию getTotalBalanceByGender(users, gender) так,
 // чтобы она возвращала общий баланс пользователей(свойство balance),
 // пол которых(свойство gender) совпадает со значением параметра gender.
-// const getTotalBalanceByGender = (users, gender) => {
-//    return [...users].filter( user => user.gender === gender ).reduce((totalBalance, user) => totalBalance+user.balance, 0)
-// };
-// console.log(getTotalBalanceByGender (users, "male"))
+const getTotalBalanceByGender = (users, gender) => {
+   return [...users].filter( user => user.gender === gender ).reduce((totalBalance, user) => totalBalance+user.balance, 0)
+};
+console.log(getTotalBalanceByGender(users, "male"))
+console.log(getTotalBalanceByGender (users, "female"))
